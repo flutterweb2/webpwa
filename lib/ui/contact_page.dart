ba309b7d35f1c00beeba3ee9abd18d92581898c6
flutterweb2 <fernandev20212@gmail.com>
2021-03-16 22:39:34 -0500

    falta usar gridf y algo responsive

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -5,41 +5,126 @@ class ContactPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       scrollDirection: Axis.vertical,
-          children: [
-            Center(
-        child: Column(
-          children: [
-            Container(
-              //height: 500,
-              width: 1300,
-              color: Colors.pink,
-              child: Text(
-                'Contact Page One',
-                style: TextStyle(fontSize: 30),
+      children: [
+        Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.pink,
+                    child: Text(
+                      'Contact Page One',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                    child: Text(
+                      'Otro contenedor Mapas',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Container(
-             //height: 500,
-              width: 1300,
-              color: Colors.blue,
-              child: Text(
-                'Otro contenedor Mapas',
-                style: TextStyle(fontSize: 30),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            Container(
-              height: 250,
-              width: 1300,
-              color: Colors.brown,
-              child: Text(
-                'Otro contenedor Localizacion',
-                style: TextStyle(fontSize: 30),
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.pink,
+                    child: Text(
+                      'Contact Page One',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                    child: Text(
+                      'Otro contenedor Mapas',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.pink,
+                    child: Text(
+                      'Contact Page One',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                    child: Text(
+                      'Otro contenedor Mapas',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.pink,
+                    child: Text(
+                      'Contact Page One',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                    child: Text(
+                      'Otro contenedor Mapas',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
-      ),
-          ],
+      ],
     );
   }
 }
