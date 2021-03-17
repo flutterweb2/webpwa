7343de08a963a335525a14f737a6d2d8e07e34cf
flutterweb2 <fernandev20212@gmail.com>
2021-03-16 21:57:34 -0500

    con menu

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -11,28 +11,134 @@ class AppView extends StatefulWidget {
 }
 
 class _AppViewState extends State<AppView> {
+  bool _isVisible = true;
+  bool switchValue = false;
+
+  //function menu options
+  void showMenuOptions() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Column(
             children: [
-              Row(
+              NavigationBar(),
+            ],
+          ),
+          Column(
+            children: [
+              Column(
                 children: [
-                  NavigationBar(),
+                  barraMenu(),
                 ],
               ),
             ],
           ),
-          Expanded(child: widget.child),
-          Column(
-            children: [
-              Container(height: 200, width: 200, color: Colors.green),
-            ],
-          )
+          Container(
+            width: 100,
+            height: 100,
+            child: widget.child),
+
+          //Expanded(child: widget.child),
         ],
+      ),
+    );
+  }
+
+  Visibility barraMenu() {
+    return Visibility(
+      visible: _isVisible,
+      child: Container(
+        margin: EdgeInsets.only(top: 0, right: 10),
+        alignment: Alignment.centerLeft,
+        color: Color(0xFF808080).withOpacity(0.5),
+        height: 440,
+        width: 70,
+        child: Column(children: <Widget>[
+          SizedBox(
+            width: 10.0,
+          ),
+          new Switch(
+            activeColor: Colors.black87,
+            onChanged: (newValue) {
+              setState(() {});
+            },
+            value: switchValue,
+          ),
+          SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                child: Icon(Icons.directions_bike),
+                elevation: 5,
+                backgroundColor: Colors.greenAccent,
+                onPressed: () {
+                  // setState(() {
+                  //   _darkMode = false;
+                  //   changeMapMode();
+                  // });
+                }),
+          ),
+          SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                child: Icon(Icons.location_searching),
+                elevation: 5,
+                backgroundColor: Colors.blueAccent,
+                onPressed: () {
+                  // getCurrentLocationCar();
+                }),
+          ),
+          SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                child: Icon(Icons.local_taxi),
+                elevation: 5,
+                backgroundColor: Colors.orangeAccent,
+                onPressed: () {
+                  //getCurrentLocation();
+                }),
+          ),
+          SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                child: Icon(
+                  Icons.notification_important,
+                  color: Colors.yellow,
+                ),
+                elevation: 5,
+                backgroundColor: Colors.purple[300],
+                onPressed: () {
+                  //getCurrentLocation();
+                }),
+          ),
+          SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+                child: Icon(
+                  Icons.phone,
+                  color: Colors.pinkAccent,
+                ),
+                elevation: 5,
+                backgroundColor: Colors.white,
+                onPressed: () => setState(() {
+                      // _launched = _makePhoneCall('tel:$_phone');
+                    })),
+          ),
+        ]),
       ),
     );
   }
--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -3,25 +3,43 @@ import 'package:flutter/material.dart';
 class ContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-            color: Colors.pink,
-            child: Text(
-              'Contact Page',
-              style: TextStyle(fontSize: 30),
+    return ListView(
+      scrollDirection: Axis.vertical,
+          children: [
+            Center(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.pink,
+              child: Text(
+                'Contact Page One',
+                style: TextStyle(fontSize: 30),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.pink,
-            child: Text(
-              'Otro contenedor Mapas',
-              style: TextStyle(fontSize: 30),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+              child: Text(
+                'Otro contenedor Mapas',
+                style: TextStyle(fontSize: 30),
+              ),
             ),
-          ),
-        ],
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.brown,
+              child: Text(
+                'Otro contenedor Mapas',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          ],
+        ),
       ),
+          ],
     );
   }
 }
